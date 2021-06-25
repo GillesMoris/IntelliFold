a3683658d913585c2a6062ca6944a95b02d7b732
Mattias Buelens <mattias.buelens@theoplayer.com>
2021-06-25 17:31:39 +0200

    Implement configure action

--- src/main/kotlin/com/github/gillesmoris/intellifold/actions/ConfigureFoldingAction.kt
+++ src/main/kotlin/com/github/gillesmoris/intellifold/actions/ConfigureFoldingAction.kt
@@ -1,10 +1,12 @@
 package com.github.gillesmoris.intellifold.actions
 
+import com.github.gillesmoris.intellifold.settings.AppSettingsConfigurable
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.options.ShowSettingsUtil
 
 class ConfigureFoldingAction : AnAction("Configure Folding") {
     override fun actionPerformed(e: AnActionEvent) {
-        // TODO
+        ShowSettingsUtil.getInstance().showSettingsDialog(e.project, AppSettingsConfigurable::class.java)
     }
 }

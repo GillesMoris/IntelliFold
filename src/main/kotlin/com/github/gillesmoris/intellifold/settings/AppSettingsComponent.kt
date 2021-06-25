925640c3f4628a250caf696b5b84ac6b8ea7aa67
Mattias Buelens <mattias.buelens@theoplayer.com>
2021-06-25 18:34:03 +0200

    Simplify

--- src/main/kotlin/com/github/gillesmoris/intellifold/settings/AppSettingsComponent.kt
+++ src/main/kotlin/com/github/gillesmoris/intellifold/settings/AppSettingsComponent.kt
@@ -1,6 +1,5 @@
 package com.github.gillesmoris.intellifold.settings
 
-import com.intellij.ui.components.JBLabel
 import com.intellij.ui.components.JBTextArea
 import com.intellij.util.ui.FormBuilder
 import javax.swing.JComponent
@@ -10,18 +9,13 @@ import javax.swing.JPanel
  * Supports creating and managing a [JPanel] for the Settings Dialog.
  */
 class AppSettingsComponent {
-    val panel: JPanel
     val regexField = JBTextArea(null, "", 10, 1);
+    val panel: JPanel = FormBuilder.createFormBuilder()
+            .addLabeledComponent("Enter one Regex per line: ", regexField, 1, true)
+            .addComponentFillVertically(JPanel(), 0)
+            .panel
 
     fun getPreferredFocusedComponent(): JComponent {
         return regexField
-    }
-
-    init {
-        panel = FormBuilder.createFormBuilder()
-                .addComponent(JBLabel("Enter one Regex per line: "))
-                .addComponent(regexField, 1)
-                .addComponentFillVertically(JPanel(), 0)
-                .panel
     }
 }

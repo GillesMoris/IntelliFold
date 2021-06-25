a4125cf4b08d1222b23f3822978aa9818fcab596
Mattias Buelens <mattias.buelens@theoplayer.com>
2021-06-25 12:43:07 +0200

    Tweak

--- src/main/kotlin/com/github/gillesmoris/intellifold/listeners/RegexFoldingBuilder.kt
+++ src/main/kotlin/com/github/gillesmoris/intellifold/listeners/RegexFoldingBuilder.kt
@@ -8,11 +8,13 @@ import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.util.TextRange
 import com.intellij.psi.PsiElement
 import com.intellij.refactoring.suggested.endOffset
+import com.intellij.util.containers.toArray
 
 class RegexFoldingBuilder() : FoldingBuilderEx(), DumbAware {
 
     override fun buildFoldRegions(root: PsiElement, document: Document, quick: Boolean): Array<FoldingDescriptor> {
         println("regexFoldingBuilder")
+        if (quick) return emptyArray()
         val descriptors = mutableListOf<FoldingDescriptor>()
         if (document.text.contains("Main")) {
             val printlineElement = root.children[3].children[11].children[9].children[4]
@@ -21,7 +23,7 @@ class RegexFoldingBuilder() : FoldingBuilderEx(), DumbAware {
             val foldingDescriptor = FoldingDescriptor(printlineElement.parent, textRange)
             descriptors.add(foldingDescriptor);
         }
-        return descriptors.toTypedArray()
+        return descriptors.toArray(FoldingDescriptor.EMPTY)
     }
 
     override fun getPlaceholderText(node: ASTNode): String {

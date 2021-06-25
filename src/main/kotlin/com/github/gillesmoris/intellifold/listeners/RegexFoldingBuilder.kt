bab7d099cbe37de53c52ab341ca466d79baa3d6b
Mattias Buelens <mattias.buelens@theoplayer.com>
2021-06-25 12:42:34 +0200

    Fold System.out.println calls in Java

--- src/main/kotlin/com/github/gillesmoris/intellifold/listeners/RegexFoldingBuilder.kt
+++ src/main/kotlin/com/github/gillesmoris/intellifold/listeners/RegexFoldingBuilder.kt
@@ -5,9 +5,10 @@ import com.intellij.lang.folding.FoldingBuilderEx
 import com.intellij.lang.folding.FoldingDescriptor
 import com.intellij.openapi.editor.Document
 import com.intellij.openapi.project.DumbAware
-import com.intellij.openapi.util.TextRange
+import com.intellij.psi.JavaRecursiveElementVisitor
 import com.intellij.psi.PsiElement
-import com.intellij.refactoring.suggested.endOffset
+import com.intellij.psi.PsiExpressionStatement
+import com.intellij.psi.PsiMethodCallExpression
 import com.intellij.util.containers.toArray
 
 class RegexFoldingBuilder() : FoldingBuilderEx(), DumbAware {
@@ -16,12 +17,17 @@ class RegexFoldingBuilder() : FoldingBuilderEx(), DumbAware {
         println("regexFoldingBuilder")
         if (quick) return emptyArray()
         val descriptors = mutableListOf<FoldingDescriptor>()
-        if (document.text.contains("Main")) {
-            val printlineElement = root.children[3].children[11].children[9].children[4]
-            val whiteSpaceElement = printlineElement.prevSibling
-            val textRange = TextRange(whiteSpaceElement.textOffset, printlineElement.endOffset)
-            val foldingDescriptor = FoldingDescriptor(printlineElement.parent, textRange)
-            descriptors.add(foldingDescriptor);
+        when (root.language.id) {
+            "JAVA" -> root.accept(object : JavaRecursiveElementVisitor() {
+                override fun visitMethodCallExpression(expression: PsiMethodCallExpression) {
+                    if (expression.methodExpression.text == "System.out.println" && expression.parent is PsiExpressionStatement) {
+                        val statement = expression.parent;
+                        val foldingDescriptor = FoldingDescriptor(statement.node, statement.textRange)
+                        descriptors.add(foldingDescriptor)
+                    }
+                    super.visitMethodCallExpression(expression)
+                }
+            })
         }
         return descriptors.toArray(FoldingDescriptor.EMPTY)
     }

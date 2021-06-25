c1667e2221713ece035e25fd979175d43511f149
George Choustoulakis <george.choustoulakis@theoplayer.com>
2021-06-25 16:14:29 +0200

    Add settings menu

---
+++ src/main/kotlin/com/github/gillesmoris/intellifold/settings/AppSettingsState.kt
@@ -0,0 +1,28 @@
+package com.github.gillesmoris.intellifold.settings
+
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.components.PersistentStateComponent
+import com.intellij.openapi.components.State
+import com.intellij.openapi.components.Storage
+import com.intellij.util.xmlb.XmlSerializerUtil
+
+/**
+ * Supports storing the application settings in a persistent way.
+ * The [State] and [Storage] annotations define the name of the data and the file name where
+ * these persistent application settings are stored.
+ */
+@State(name = "com.github.gillesmoris.intellifold.settings.AppSettingsState", storages = [Storage("SdkSettingsPlugin.xml")])
+class AppSettingsState : PersistentStateComponent<AppSettingsState?> {
+    override fun getState(): AppSettingsState? {
+        return this
+    }
+
+    override fun loadState(state: AppSettingsState) {
+        XmlSerializerUtil.copyBean(state, this)
+    }
+
+    companion object {
+        val instance: AppSettingsState
+            get() = ApplicationManager.getApplication().getService(AppSettingsState::class.java)
+    }
+}

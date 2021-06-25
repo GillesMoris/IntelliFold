7d1e4c0fcdaaaa5fd33324782323c55264eaf475
Mattias Buelens <mattias.buelens@theoplayer.com>
2021-06-25 17:07:31 +0200

    Wrap in SplitButtonAction

---
+++ src/main/kotlin/com/github/gillesmoris/intellifold/actions/SplitFoldActionGroup.kt
@@ -0,0 +1,15 @@
+package com.github.gillesmoris.intellifold.actions
+
+import com.intellij.openapi.actionSystem.Presentation
+import com.intellij.openapi.actionSystem.SplitButtonAction
+import com.intellij.openapi.actionSystem.ex.CustomComponentAction
+import javax.swing.JComponent
+
+class SplitFoldActionGroup : DelegateActionGroup(SplitButtonAction(FoldActionGroup())), CustomComponentAction {
+    override fun createCustomComponent(presentation: Presentation, place: String): JComponent {
+        if (delegate is CustomComponentAction) {
+            return delegate.createCustomComponent(presentation, place)
+        }
+        return super.createCustomComponent(presentation, place)
+    }
+}

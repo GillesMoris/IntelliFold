4553309ed8f97efaf7b6110b0b8ea2e3bd9a54b6
Mattias Buelens <mattias.buelens@theoplayer.com>
2021-06-28 11:49:31 +0200

    Fix deprecation warnings

--- src/main/kotlin/com/github/gillesmoris/intellifold/actions/ToggleFoldingAction.kt
+++ src/main/kotlin/com/github/gillesmoris/intellifold/actions/ToggleFoldingAction.kt
@@ -9,9 +9,6 @@ import com.intellij.util.FileContentUtil
 const val ENABLE_TEXT = "Enable Folding"
 const val DISABLE_TEXT = "Disable Folding"
 
-val nonActiveIcon = IconLoader.getIcon("/icons/intellifold.svg")
-val activeIcon = IconLoader.getIcon("/icons/intellifold_live.svg")
-
 class ToggleFoldingAction : AnAction(ENABLE_TEXT, "", nonActiveIcon) {
 
     var enabled = false
@@ -25,5 +22,10 @@ class ToggleFoldingAction : AnAction(ENABLE_TEXT, "", nonActiveIcon) {
     override fun update(e: AnActionEvent) {
         e.presentation.text = if (enabled) DISABLE_TEXT else ENABLE_TEXT
         e.presentation.icon = if (enabled) activeIcon else nonActiveIcon
+    }
+
+    companion object {
+        val nonActiveIcon = IconLoader.getIcon("/icons/intellifold.svg", ToggleFoldingAction::class.java)
+        val activeIcon = IconLoader.getIcon("/icons/intellifold_live.svg", ToggleFoldingAction::class.java)
     }
 }
